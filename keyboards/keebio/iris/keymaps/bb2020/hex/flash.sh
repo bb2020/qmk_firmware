296aec0d903a5286aaffa46c457091c4a87d853f
bb2020 <bb2020@users.noreply.github.com>
2020-10-21 22:55:53 +0300

    iris

--- keyboards/keebio/iris/keymaps/bb2020/hex/flash.sh
+++ keyboards/keebio/iris/keymaps/bb2020/hex/flash.sh
@@ -1,3 +1,3 @@
 sudo dfu-programmer atmega32u4 erase --force
-sudo dfu-programmer atmega32u4 flash ./hex/keebio_iris_rev4_bb2020.hex
+sudo dfu-programmer atmega32u4 flash ./keebio_iris_rev4_bb2020.hex
 sudo dfu-programmer atmega32u4 reset
