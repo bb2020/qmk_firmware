ced601673fd5542b8cad33438d77955ef0841306
bb2020 <bb2020@users.noreply.github.com>
2020-10-30 12:43:06 +0300

    updated

--- keyboards/keebio/iris/keymaps/bb2020/build.sh
+++ keyboards/keebio/iris/keymaps/bb2020/build.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+
 qmk json2c keebio_iris_rev4_layout_mine.json >keymap.c
 qmk compile -kb keebio/iris/rev4 -km bb2020
--- keyboards/keebio/iris/keymaps/bb2020/hex/flash.sh
+++ keyboards/keebio/iris/keymaps/bb2020/hex/flash.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 sudo dfu-programmer atmega32u4 erase --force
 sudo dfu-programmer atmega32u4 flash ./keebio_iris_rev4_bb2020.hex
 sudo dfu-programmer atmega32u4 reset
