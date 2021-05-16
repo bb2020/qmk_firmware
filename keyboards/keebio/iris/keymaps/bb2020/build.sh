4fcc0dd73509f49d52b8e13c15fd7d19c1aefe96
bb2020 <bb2020@users.noreply.github.com>
2021-05-16 16:43:18 +0300

    cleanup

--- keyboards/keebio/iris/keymaps/bb2020/build.sh
+++ keyboards/keebio/iris/keymaps/bb2020/build.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
-qmk json2c keebio_iris_rev4_layout_mine.json >keymap.c
+qmk json2c ./keebio_iris_rev4_layout_mine.json >keymap.c
 qmk compile -kb keebio/iris/rev4 -km bb2020
+cp ../../../../../keebio_iris_rev4_bb2020.hex build/
